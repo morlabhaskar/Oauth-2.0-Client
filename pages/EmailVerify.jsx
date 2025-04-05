09b5ba5311df633baa131e6c015dd596bbbd2ac4
morlabhaskar <morlabhaskar306@gmail.com>
2025-04-06 01:01:20 +0530

    After Complete a Application

--- pages/EmailVerify.jsx
+++ pages/EmailVerify.jsx
@@ -1,11 +1,16 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import { assets } from '../src/assets/assets'
 import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
+//for ldrs loader
+import { DotWave } from 'ldrs/react'
+import 'ldrs/react/DotWave.css'
+
 const EmailVerify = () => {
+  const [loading, setLoading] = useState(false)
   const navigate = useNavigate()
 
   axios.defaults.withCredentials = true;
@@ -38,19 +43,23 @@ const EmailVerify = () => {
   }
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
+    setLoading(true)
     const otp = inputRefs.current.map(input => input.value).join('')
     try {
       const {data} = await axios.post(backendUrl + '/api/auth/verify-account', {otp})
       if(data.success){
+        setLoading(false)
         toast.success(data.message)
         getUserData()
         navigate('/')
       }
       else{
+        setLoading(false)
         toast.error(data.message)
       }
     } catch (error) {
+      setLoading(false)
       toast.error(error.message)
       console.log(error.message)
     }
@@ -63,10 +72,10 @@ const EmailVerify = () => {
 
   return (
     <div className='flex justify-center items-center min-h-screen bg-gradient-to-br from-blue-200 to-purple-400'>
-      <img onClick={()=>navigate('/')} src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
-      <form onSubmit={handleSubmit} className='bg-slate-900 p-8 rounded-lg shadow-lg w-96 text-sm'>
-        <h1 className='text-white text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
-        <p className='text-center mb-6 text-indigo-300'>Enter the 6-digit code sent to your regiter Email Id.</p>
+      <h1 onClick={() => navigate('/')} className="absolute cursor-pointer top-5 left-8 self-center md:text-3xl text-center text-lg font-semibold whitespace-nowrap bg-gradient-to-br from-blue-600 via-purple-500 to-red-600 bg-clip-text text-transparent mb-2">oAuth</h1>
+      <form onSubmit={handleSubmit} className='bg-transparent border-[3px] border-zinc-800 p-8 rounded-lg shadow-lg w-96 text-sm'>
+        <h1 className='text-zinc-800 text-2xl font-semibold text-center mb-4'>Email Verify OTP</h1>
+        <p className='text-center mb-6 text-indigo-700'>Enter the 6-digit code sent to your regiter Email Address.</p>
         <div className='flex justify-between mb-8' onPaste={handlePaste}>
         {Array(6).fill(0).map((_,index)=>(
           <input key={index} type="text" 
@@ -78,7 +87,14 @@ const EmailVerify = () => {
           />
         ))}
         </div>
-        <button className='w-full py-3 bg-gradient-to-r from-indigo-500 to-indigo-900 text-white rounded-full cursor-pointer'>Verify Email</button>
+        <button className='w-full py-3 bg-gradient-to-br from-blue-600 via-purple-500 to-red-400 text-white rounded-full cursor-pointer'>
+          {loading
+              ? <DotWave
+                size="30"
+                speed="2"
+                color="white" />
+              : "Verify Email"}
+        </button>
       </form>
     </div>
   )
--- pages/Login.jsx
+++ pages/Login.jsx
@@ -5,80 +5,113 @@ import { AppContext } from '../context/AppContext'
 import axios from 'axios'
 import { toast } from 'react-toastify'
 
+//for ldrs loader
+import { DotWave } from 'ldrs/react'
+import 'ldrs/react/DotWave.css'
+
+
+
 const Login = () => {
 
   const navigate = useNavigate()
 
-  const {backendUrl,setIsLoggedin,getUserData} = useContext(AppContext)
+  const { backendUrl, setIsLoggedin, getUserData } = useContext(AppContext)
 
   const [state, setState] = useState("Sign Up")
   const [name, setName] = useState("")
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
+  const [loading, setLoading] = useState(false)
 
   const submitHandler = async (e) => {
     try {
+
       e.preventDefault();
+      setLoading(true)
       axios.defaults.withCredentials = true;
-      if(state === 'Sign Up'){
-        const {data} = await axios.post(backendUrl + '/api/auth/register',{name,email,password})
-        if(data.success){
+      if (state === 'Sign Up') {
+        const { data } = await axios.post(backendUrl + '/api/auth/register', { name, email, password })
+        if (data.success) {
           setIsLoggedin(true)
+          setLoading(false)
+          toast.success(data.message)
           getUserData()
           navigate('/')
         }
-        else{
+        else {
           toast.error(data.message)
+          console.log(data.message)
+          setLoading(false)
         }
       }
-      else{
-        const {data} = await axios.post(backendUrl + '/api/auth/login',{email,password})
-        // const {data} = await axios.post(`${backendUrl}/api/auth/login`,{email,password})
-        if(data.success){
+      else {
+        const { data } = await axios.post(backendUrl + '/api/auth/login', { email, password })
+        if (data.success) {
           setIsLoggedin(true)
+          setLoading(false)
+          toast.success(data.message)
           getUserData()
           navigate('/')
+
         }
-        else{
+        else {
           toast.error(data.message)
+          console.log(data.message)
+          setLoading(false)
         }
       }
     } catch (error) {
       toast.error(error.message)
+      console.log(error.message)
+      setLoading(false)
     }
   }
 
   return (
-    <div>
-      <div>
-        <h2>{state === 'Sign Up' ? 'Create Account' : 'Login your Account'}</h2>
-        <p>{state === 'Sign Up' ? 'Create your Account' : 'Login your Account'}</p>
+    <div className='bg-white min-h-[100vh] flex items-center justify-center'>
+      <div className='p-4 rounded-xl border-[3px] border-zinc-800 shadow-2xl'>
+        <h1 className="self-center md:text-3xl text-center text-lg font-semibold whitespace-nowrap bg-gradient-to-br from-blue-600 via-purple-500 to-red-600 bg-clip-text text-transparent mb-2">oAuth</h1>
+
+        <h2 className='text-2xl font-bold text-teal-700 text-center'>{state === 'Sign Up' ? 'Create Account' : 'Login your Account'}</h2>
+        <p className='text-md text-center mb-2'>{state === 'Sign Up' ? 'Create your Account' : 'Login your Account'}</p>
 
         <form onSubmit={submitHandler}>
           {state === 'Sign Up' && (
-            <div className='flex items-center gap-1'>
-              <img src={assets.person_icon} alt="" />
-              <input type="text" placeholder='Full Name' onChange={e => setName(e.target.value)} value={name} required />
-            </div>)}
+            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-zinc-700'>
+              <img src={assets.person_icon} alt="" className='w-3 h-3' />
+              <input type="text" className='bg-transparent outline-none text-white' placeholder='Full Name' onChange={e => setName(e.target.value)} value={name} required />
+            </div>
+          )}
 
 
-          <div className='flex items-center gap-1'>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-zinc-700'>
             <img src={assets.mail_icon} alt="" />
-            <input type="email" placeholder='Email' onChange={e => setEmail(e.target.value)} value={email} required />
+            <input type="email" placeholder='Email' className='bg-transparent outline-none text-white' onChange={e => setEmail(e.target.value)} value={email} required />
           </div>
 
-          <div className='flex items-center gap-1'>
+          <div className='mb-2 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-zinc-700'>
             <img src={assets.lock_icon} alt="" />
-            <input type="password" placeholder='Password' onChange={e => setPassword(e.target.value)} value={password} required />
+            <input type="password" placeholder='Password' className='bg-transparent outline-none text-white' onChange={e => setPassword(e.target.value)} value={password} required />
           </div>
-          <p className='' onClick={()=>navigate('/reset-password')}>Forgot Password?</p>
-          <button>{state === 'Sign Up' ? 'Sign Up' : 'Login'}</button>
+
+          <p className='text-sm text-red-500 cursor-pointer' onClick={() => navigate('/reset-password')}>Forgot Password?</p>
+
+          <button className='text-ms text-center w-full bg-teal-800 text-white py-2 my-2 rounded-full cursor-pointer hover:scale-105 transition-all duration-400'>
+            {loading 
+            ? <p className='flex items-center gap-2 justify-center'><span>Loading</span>
+            <DotWave
+            size="30"
+            speed="2"
+            color="white"/> 
+            </p>
+            : state === 'Sign Up' ? 'Sign Up' : 'Login'}
+          </button>
 
         </form>
 
-        {state === 'Sign Up' 
-        ? (<p>Already have an account? {' '} <span className='' onClick={()=>setState('Login')}>Login here</span></p>) 
-          : (<p>Don't have an account? {' '} <span className='' onClick={()=>setState('Sign Up')}>Signup here</span></p>)}
+        {state === 'Sign Up'
+          ? (<p className='text-sm text-center'>Already have an account? {' '} <span className='text-blue-700 cursor-pointer' onClick={() => setState('Login')}>Login here</span></p>)
+          : (<p className='text-sm text-center' >Don't have an account? {' '} <span className='text-blue-700 cursor-pointer' onClick={() => setState('Sign Up')}>Signup here</span></p>)}
 
 
 
